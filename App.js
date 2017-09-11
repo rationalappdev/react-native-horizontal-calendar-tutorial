2d7eebcefa7ba91aae7d7c254cda04a77c8c10e3
Konstantin Shkut <Konstantin.Shkut@gmail.com>
2017-09-10 17:51:20 -0700

    updated App.js to use Calendar component

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
+// @flow
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  StatusBar,
+  View,
+} from 'react-native';
+import Calendar from './src/calendar/Calendar';
+import type Moment from 'moment';
 
 export default class App extends React.Component {
+
+  onSelectDate = (date: Moment) => {
+    alert(date.calendar());
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <StatusBar hidden={true} />
+        <Calendar onSelectDate={this.onSelectDate} />
       </View>
     );
   }
@@ -16,8 +28,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#3F53B1',
+    paddingTop: 20,
   },
 });
