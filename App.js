905db66ae85181cd26099db90aad3b248c38ed31
kewilson <kewilson@users.noreply.github.com>
2018-05-09 23:06:36 -0500

    changes for wip of calendar

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-// @flow
+// @flow strict-local
+
+"use strict";
 
 import React from 'react';
 import {
-  StyleSheet,
-  StatusBar,
-  View,
+    StyleSheet,
+    StatusBar,
+    View,
 } from 'react-native';
 import faker from 'faker';
 import moment from 'moment';
@@ -13,54 +15,54 @@ import Events from './src/events/Events';
 import type Moment from 'moment';
 
 export type EventType = {
-  date: Moment,
-  title: string,
-  description: string,
-  image: string,
+    date: Moment,
+    title: string,
+    description: string,
+    image: string,
 };
 
 // Generate fake event data
 const FAKE_EVENTS: Array<EventType> = (() => {
-  const startDay = moment().subtract(5, 'days').startOf('day');
-  return [...new Array(64)].map(_ => ({
-    date: startDay.add(4, 'hours').clone(),
-    title: faker.company.companyName(),
-    description: faker.lorem.sentence(),
-    // use random dimensions to get random urls
-    image: faker.image.nightlife(Math.floor(Math.random() * 200) + 100, Math.floor(Math.random() * 200) + 100),
-  }));
+    const startDay = moment().subtract(5, 'days').startOf('day');
+    return [...new Array(64)].map(_ => ({
+        date: startDay.add(4, 'hours').clone(),
+        title: faker.company.companyName(),
+        description: faker.lorem.sentence(),
+        // use random dimensions to get random urls
+        image: faker.image.nightlife(Math.floor(Math.random() * 200) + 100, Math.floor(Math.random() * 200) + 100),
+    }));
 })();
 
 // Filter events by date
 const filterEvents = (date: Moment): ?Array<EventType> =>
-  FAKE_EVENTS.filter(event => event.date.isSame(date, 'day'));
+    FAKE_EVENTS.filter(event => event.date.isSame(date, 'day'));
 
 export default class App extends React.Component {
 
-  state = {
-    events: filterEvents(moment()),
-  };
+    state = {
+        events: filterEvents(moment()),
+    };
 
-  onSelectDate = (date: Moment) => {
-    this.setState({ events: filterEvents(date) });
-  };
+    onSelectDate = (date: Moment) => {
+        this.setState({events: filterEvents(date)});
+    };
 
-  render() {
-    const { events } = this.state;
-    return (
-      <View style={styles.container}>
-        <StatusBar hidden={true} />
-        <Calendar onSelectDate={this.onSelectDate} />
-        <Events events={events} />
-      </View>
-    );
-  }
+    render() {
+        const {events} = this.state;
+        return (
+            <View style={styles.container}>
+                <StatusBar hidden={true}/>
+                <Calendar onSelectDate={this.onSelectDate}/>
+                <Events events={events}/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3F53B1',
-    paddingTop: 20,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#3F53B1',
+        paddingTop: 20,
+    },
 });
--- src/events/Events.js
+++ src/events/Events.js
@@ -1,37 +1,39 @@
-// @flow
+// @flow strict-local
 
-import React, { Component } from 'react';
+"use strict";
+
+import React, {Component} from 'react';
 import {
-  ScrollView,
-  StyleSheet,
-  View,
+    ScrollView,
+    StyleSheet,
+    View,
 } from 'react-native';
 import Event from './Event';
-import type { EventType } from '../../App';
+import type {EventType} from '../../App';
 
 export default class Events extends Component {
 
-  props: {
-    events: ?Array<EventType>,
-  };
+    props: {
+        events: ?Array<EventType>,
+    };
 
-  render() {
-    const { events } = this.props;
-    return (
-      <View style={styles.container}>
-        <ScrollView>
-          {events && events.map((event, index) =>
-            <Event event={event} key={index} />)}
-        </ScrollView>
-      </View>
-    );
-  }
+    render() {
+        const {events} = this.props;
+        return (
+            <View style={styles.container}>
+                <ScrollView>
+                    {events && events.map((event, index) =>
+                        <Event event={event} key={index}/>)}
+                </ScrollView>
+            </View>
+        );
+    }
 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#243177',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#243177',
+    },
 });
